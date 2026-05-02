07053222d78f5a90e5fcef1cf96e4bd9d939064d
vab2410tb-ui <vab2410tb@gmail.com>
2026-05-02 10:56:44 +0700

    fix insecure deserialization vulnerability

--- app/admin/import_user.php
+++ app/admin/import_user.php
@@ -1,16 +1,22 @@
 <?php
-    include('../includes/utils.php');
+    session_start();
+
+    if (!isset($_SESSION['isadmin']) || $_SESSION['isadmin'] !== true) {
+        header("location: /index.php");
+        die();
+    }
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $userObj = $_POST['userobj'];
-        if ($userObj !== "") {
-            $user = unserialize($userObj);
+        $username = trim($_POST['username'] ?? '');
+        $password = trim($_POST['password'] ?? '');
+        $description = trim($_POST['description'] ?? '');
+
+        if ($username !== "" && $password !== "") {
             include('../includes/db_connect.php');
-            $ret = pg_prepare($db,
-                "importuser_query", "insert into users (username, password, description) values ($1, $2, $3)");
-            $ret = pg_execute($db, "importuser_query", array($user->username,$user->password,$user->description));
+            $ret = pg_prepare($db, "importuser_query", "insert into users (username, password, description) values ($1, $2, $3)");
+            $ret = pg_execute($db, "importuser_query", array($username, $password, $description));
+            header("location: /index.php");
+            die();
         }
     }
-    header('location:/index.php');
-    die();
 ?>
